5da8b2d1b80004bafa340789dc07fbd4190b8acd
gisellehargrove <gisellehargrove@gmail.com>
2019-06-18 15:49:57 -0400

    added event listeners to 10 elements

--- js/index.js
+++ js/index.js
@@ -1,1 +1,52 @@
 // Your code goes here
+// Add 10 unique event listeners
+const logoHeading = document.getElementsByClassName('logo-heading')[0];
+
+logoHeading.addEventListener('mouseenter', () => {
+  console.log('I am clicking');
+});
+
+const homeLink = document.getElementsByClassName('nav-link1')[0];
+homeLink.addEventListener('mouseenter', () => {
+  console.log('I am listening');
+});
+
+const aboutLink = document.getElementsByClassName('nav-link2')[0];
+aboutLink.addEventListener('mouseenter', () => {
+  console.log('I am listening');
+});
+
+const blogLink = document.getElementsByClassName('nav-link3')[0];
+blogLink.addEventListener('mouseenter', () => {
+  console.log('I am listening');
+});
+
+const contactLink = document.getElementsByClassName('nav-link4')[0];
+contactLink.addEventListener('mouseenter', () => {
+  console.log('I am listening');
+});
+
+const images = document.getElementsByTagName('img');
+
+const busImg = images[0],
+adventureImg = images[1],
+      funImg = images[2],
+    slideImg = images[3];
+
+busImg.addEventListener('mouseenter', () => {
+  console.log('I am clicking');
+});
+
+const headerText = document.getElementsByTagName('h2');
+
+
+// const s = document.getElementsByClassName('')[0];
+//
+//
+// const s = document.getElementsByClassName('')[0];
+//
+//
+// const s = document.getElementsByClassName('')[0];
+//
+//
+// const s = document.getElementsByClassName('')[0];
